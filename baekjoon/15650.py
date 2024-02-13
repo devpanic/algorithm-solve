fe0de569f84fc7183c51dc6ee8132fd185d183dd
devpanic <yeon.dev.8@gmail.com>
2024-02-13 09:39:44 +0900

    240213

---
+++ baekjoon/15650.py
@@ -0,0 +1,19 @@
+import sys
+
+sys.setrecursionlimit(1000)
+n, depth = map(int, sys.stdin.readline().split())
+result = []
+
+
+def dfs(k):
+  if len(result) == depth:
+    print(' '.join(map(str, result)))
+    return
+  for i in range(k, n + 1):
+    if i not in result:
+      result.append(i)
+      dfs(i)
+      result.pop()
+
+
+dfs(1)
---
+++ baekjoon/15651.py
@@ -0,0 +1,18 @@
+import sys
+
+sys.setrecursionlimit(1000)
+n, m = map(int, sys.stdin.readline().split())
+result = []
+
+
+def dfs():
+  if len(result) == m:
+    print(' '.join(map(str, result)))
+    return
+  for i in range(1, n + 1):
+    result.append(i)
+    dfs()
+    result.pop()
+
+
+dfs()
